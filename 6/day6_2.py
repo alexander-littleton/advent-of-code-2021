2f95415edd4d21ca2eb9f944e89bdfcaa526ea96
Alex Littleton <littale12@gmail.com>
2021-12-07 10:02:23 -0500

    day 7 combined and day 6 changes

--- 6/day6_2.py
+++ 6/day6_2.py
@@ -2,21 +2,21 @@ school = []
 passing_days = 256
 total = len(school)
 
-birth_map = {}
+birthdays = [0] * 256
 for day in range(passing_days):
-    birth_map[day] = 0
+    birthdays[day] = 0
 
 for fish in school:
     day = fish
     while day < passing_days:
-        birth_map[day] += 1
+        birthdays[day] += 1
         day += 7
 
 for day in range(passing_days):
-    babies = birth_map[day]
+    babies = birthdays[day]
     day_iter = day + 9
     while day_iter < passing_days:
-        birth_map[day_iter] += babies
+        birthdays[day_iter] += babies
         day_iter += 7
     total += babies
 print(total)
